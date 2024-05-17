e32182a303be0eb88bb9212a613e6615869cd96b
JeanPhiH <jp.herfeld@gmail.com>
2024-05-17 10:43:26 +0200

    animated navbar ok

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,20 +1,39 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Navbar() {
 	return (
-		<nav className="flex flex-row p-2">
-			<ul className="flex gap-1 md:gap-5">
-				<li className="relative group p-2">
-          <div className="absolute inset-0 border-highlight group-hover:border-t-2 rounded-full group-hover:animate-spin-quick"></div>
-          <Image className="text-light md:hidden relative z-10" src="/icons/home.svg" alt="HomePage" width={24} height={24} />
-          <span className="hidden md:flex">Home</span>
-        </li>
-				<li><Image className="md:hidden" src="/icons/project.svg" alt="HomePage" width={24} height={24} /><span className="hidden md:flex">Projects</span></li>
-				<li><Image className="md:hidden" src="/icons/testimonial.svg" alt="HomePage" width={24} height={24} /><span className="hidden md:flex">Testimonials</span></li>
-				<li><Image className="md:hidden" src="/icons/cv.svg" alt="HomePage" width={24} height={24} /><span className="hidden md:flex">Resume</span></li>
-				<li><Image className="md:hidden" src="/icons/mail.svg" alt="HomePage" width={24} height={24} /><span className="hidden md:flex">E-mail</span></li>
-				<li><Image className="text-light md:hidden" src="/icons/linkedin.svg" alt="HomePage" width={24} height={24} /><span className="hidden md:flex">LinkedIn</span></li>
-			</ul>
+		<nav className="flex flex-row items-center jus gap-1 md:gap-5 p-2">
+			<Link href="/" className="flex flex-col items-center justify-center md:w-24 md:h-24 relative group p-2">
+				<div className="rounded-full absolute inset-0 border-highlight group-hover:border-t-2 group-hover:animate-spin-quick"></div>
+				<Image className="text-light relative z-10" src="/icons/home.svg" alt="HomePage" width={24} height={24} />
+				<span className="hidden md:flex">Accueil</span>
+			</Link>
+			<Link href="/" className="flex flex-col items-center justify-center md:w-24 md:h-24 relative group p-2">
+				<div className="rounded-full absolute inset-0 border-highlight group-hover:border-t-2 group-hover:animate-spin-quick"></div>
+				<Image className="text-light relative z-10" src="/icons/project.svg" alt="HomePage" width={24} height={24} />
+				<span className="hidden md:flex">Projets</span>
+			</Link>
+			<Link href="/" className="flex flex-col items-center justify-center md:w-24 md:h-24 relative group p-2">
+				<div className="rounded-full absolute inset-0 border-highlight group-hover:border-t-2 group-hover:animate-spin-quick"></div>
+				<Image className="text-light relative z-10" src="/icons/testimonial.svg" alt="HomePage" width={24} height={24} />
+				<span className="hidden md:flex">Avis</span>
+			</Link>
+			<Link href="/" className="flex flex-col items-center justify-center md:w-24 md:h-24 relative group p-2">
+				<div className="rounded-full absolute inset-0 border-highlight group-hover:border-t-2 group-hover:animate-spin-quick"></div>
+				<Image className="text-light relative z-10" src="/icons/cv.svg" alt="HomePage" width={24} height={24} />
+				<span className="hidden md:flex">CV</span>
+			</Link>
+			<Link href="/" className="flex flex-col items-center justify-center md:w-24 md:h-24 relative group p-2">
+				<div className="rounded-full absolute inset-0 border-highlight group-hover:border-t-2 group-hover:animate-spin-quick"></div>
+				<Image className="text-light relative z-10" src="/icons/mail.svg" alt="HomePage" width={24} height={24} />
+				<span className="hidden md:flex">E-mail</span>
+			</Link>
+			<Link href="/" className="flex flex-col items-center justify-center md:w-24 md:h-24 relative group p-2">
+				<div className="rounded-full absolute inset-0 border-highlight group-hover:border-t-2 group-hover:animate-spin-quick"></div>
+				<Image className="text-light relative z-10" src="/icons/linkedin.svg" alt="HomePage" width={24} height={24} />
+				<span className="hidden md:flex">LinkedIn</span>
+			</Link>
 		</nav>
 	);
 }
