86361365900d70cb5aa6ce37a3da3a1c532cf244
JeanPhiH <jp.herfeld@gmail.com>
2024-05-21 11:42:17 +0200

    add bio

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -3,7 +3,19 @@
 export default function About () {
 	return (
 		<section className="px-4 my-10 flex flex-col md:flex-row-reverse md:justify-between">
-			<h2>Qui suis-je ?</h2>
+			<article>
+				<h2>Qui suis-je ?</h2>
+				<p className="mt-5">Je m’appelle</p>
+				<p className="text-highlight text-4xl font-bold">Jean-Philippe Herfeld</p>
+				<p>et je suis développeur web junior.</p>
+				<p>J’ai étudié chez OpenClassrooms et j’y ai aimé l’importance accordée à la <span className="text-highlight">pratique</span> et à l’<span className="text-highlight">autonomie</span>.</p>
+				<p>Je suis curieux de tout et aime les challenges que m’offre la vie.</p>
+				<p>Ma maxime du moment: </p>
+				<blockquote className="italic">C’est dans la contrainte que nait l’inspiration.</blockquote>
+			</article>
+			<div className="mt-10">
+				<div className="frametl"></div>
+			</div>
 		</section>
 	)
 }
