e97768663fb647a09e65f099d4c460fdb77a3f0c
JeanPhiH <jp.herfeld@gmail.com>
2024-05-27 11:04:48 +0200

    responsive technos ok

--- src/components/Technos.tsx
+++ src/components/Technos.tsx
@@ -2,15 +2,15 @@ import Image from 'next/image';
 
 export default function Technos() {
 	return (
-		<section className="hidden mt-32 md:flex flex-col items-center md:w-2/3 md:mx-auto">
+		<section className="p-10 rounded-xl shadow-lg shadow-light/50 bg-light/15 mt-32 flex flex-col items-center md:w-2/3 md:mx-auto">
 			<h2 className="text-highlight">Portfolio réalisé avec</h2>
-			<div className="my-10 flex flex-row gap-4 md:gap-6">
-				<Image className="relative top-24 right-20 lg:w-14" src="/icons/nextjs-stroke.svg" alt="NextJS" width={50} height={50} />
-				<Image className="relative top-6 right-12 lg:w-14" src="/icons/typescript-icon.svg" alt="TypeScript" width={50} height={50} />
-				<Image className="relative top-7 left-8 lg:w-14" src="/icons/tailwind-css.svg" alt="TailwindCSS" width={50} height={50} />
-				<Image className="relative top-24 left-20 lg:w-14" src="/icons/mongodb-icon.svg" alt="MongoDB" width={50} height={50} />
+			<div className="h-36">
+				<Image className="relative top-28 right-28 lg:w-14 lg:top-36 lg:right-48" src="/icons/nextjs-stroke.svg" alt="NextJS" width={30} height={30} />
+				<Image className="relative top-10 right-12 lg:w-14 lg:top-5 lg:right-20" src="/icons/typescript-icon.svg" alt="TypeScript" width={30} height={30} />
+				<Image className="relative top-5 left-10 lg:w-14 lg:-top-6 lg:left-20" src="/icons/tailwind-css.svg" alt="TailwindCSS" width={30} height={30} />
+				<Image className="relative top-8 left-28 lg:w-14 lg:-top-2 lg:left-48" src="/icons/mongodb-icon.svg" alt="MongoDB" width={30} height={30} />
 			</div>
-			<svg className="w-[450px]" viewBox="0 30 500 125">
+			<svg className="w-[300px] lg:w-[500px]" viewBox="0 30 500 125">
 				<defs>
 					<path id="curve" fill="#ffe5cc" d="M73.2,148.6c4-6.1,65.5-96.8,178.6-95.6c111.3,1.2,170.8,90.3,175.1,97" />
 				</defs>
