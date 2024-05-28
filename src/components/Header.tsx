daab6a9898c18372b22de140ef494ca52781ce62
JeanPhiH <jp.herfeld@gmail.com>
2024-05-28 09:04:15 +0200

    hide burger menu with an outside click

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,19 +2,35 @@
 
 import MenuItem from "./MenuItem";
 import Image from "next/image";
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 
 export default function Header() {
 
 	// Display burger menu by changing nav style
 	const defaultNavStyle = "hidden fixed bg-dark z-10 bg-dark top-9 right-0 md:static md:flex flex-col md:flex-row items-start md:items-center justify-center gap-1 md:gap-2 p-2";
 	const [navStyle, setNavStyle] = useState(defaultNavStyle);
+
+	const burgerRef = useRef<HTMLImageElement | null>(null);
+
 	const displayBurger = () => {
 		if (navStyle === defaultNavStyle) setNavStyle(defaultNavStyle.replace("hidden", "flex"));
         else setNavStyle(defaultNavStyle);
-
-
 	}
+
+	const handleClickOutside = (event: MouseEvent) => {
+    if (burgerRef.current && 
+        !burgerRef.current.contains(event.target as Node)) {
+      setNavStyle(defaultNavStyle);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("click", handleClickOutside);
+    return () => {
+      document.removeEventListener("click", handleClickOutside);
+    };
+  }, [navStyle]);
+
 	return (
 		<header className="z-10 flex bg-dark justify-between px-4 items-center border-b-2 border-b-light">
 			<h1 className="md:text-2xl font-bold">JeanPhiH’s Portfolio</h1>
@@ -38,7 +54,7 @@ export default function Header() {
 					src="/icons/linkedin.svg"
 				/>
 			</nav>
-			<Image src="/icons/burger-menu.svg" alt="Menu Burger" width={24} height={24} className="fixed right-0 top-0 z-10 bg-dark md:static md:hidden" onClick={displayBurger}/>
+			<Image ref={burgerRef} src="/icons/burger-menu.svg" alt="Menu Burger" width={34} height={34} className="fixed right-0 top-0 z-10 bg-dark md:static md:hidden" onClick={displayBurger}/>
 		</header>
 	);
 }
