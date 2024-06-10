3aeaa668d10f483c422369fbfe92db0bf5eb8c04
JeanPhiH <jp.herfeld@gmail.com>
2024-06-10 14:55:37 +0200

    fixed home link in not-found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,11 +1,18 @@
 import Link from "next/link";
-import MenuItem from "@/components/MenuItem"
+import MenuItem from "@/components/MenuItem";
 
 export default function NotFound() {
 	return (
 		<section className="flex flex-col items-center">
 			<h3>La page recherchée n’existe pas</h3>
-			<MenuItem title="Accueil" href="/" target="_self" src="/icons/home.svg" size={24}/>
+			<MenuItem
+				title="Accueil"
+				href="/"
+				target="_self"
+				src="/icons/home.svg"
+				width={24}
+				height={24}
+			/>
 		</section>
 	);
 }
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -21,7 +21,7 @@ export default function Projects() {
 							height={492}
 							className="rounded-xl lg:absolute object-cover w-full h-[250px] lg:h-full lg:group-hover/project:scale-[0.33] duration-500 origin-top-left"
 						/>
-						<h3 className="bg-dark/75 text-light w-full lg:w-[150px] p-3 rounded-xl lg:absolute left-5 bottom-5 text-center lg:group-hover/project:left-20 lg:group-hover/project:bottom-[150px] duration-500">
+						<h3 className="bg-dark/75 text-light w-full lg:w-[150px] p-3 lg:rounded-xl lg:absolute left-5 bottom-5 text-center lg:group-hover/project:left-20 lg:group-hover/project:bottom-[150px] duration-500">
 							{project.title}
 						</h3>
 						<div className="flex justify-center gap-3 bg-dark/75 lg:w-[150px] p-3 lg:absolute left-[200px] bottom-5 text-center lg:group-hover/project:left-20 duration-500  lg:rounded-full">
